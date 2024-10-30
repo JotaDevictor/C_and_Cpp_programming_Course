ebb7c6bbacde456ed03bf357bb921dd3a8988291
João Victor <joaovictor.osantosdev@gmail.com>
2024-10-30 11:07:57 -0300

    Update and rename main.c to AULA_3.c
    comments & escape sequences 💬

---
+++ AULA_3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    // Para uma linha de comentário apenas
+
+    /*
+      Para multiplas linhas de comentários
+
+      #Escape sequences:
+        \n = nova linha 
+        \t = TAB
+        \" ou \' = para citações
+    */
+    
+    printf("I\nlike\nPizza!\n");
+    printf("1\t2\t3\t4\t5\t6\n");
+    printf("\"I like Pizza\"  - Abraham Lincoln probably");
+    return 0;
+}
--- main.c
+++
@@ -1,8 +0,0 @@
-#include  <stdio.h>
-
-int main(){
-  printf("Hello, Wolrd!\n");
-  printf("I like Pizza!!\n");
-  printf("Its really good!!!");
-  return 0;
-}
