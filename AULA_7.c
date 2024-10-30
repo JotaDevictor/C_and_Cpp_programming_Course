e6bf2578d3b2201c5c042692402e881ab3ffe604
João Victor <joaovictor.osantosdev@gmail.com>
2024-10-30 12:59:21 -0300

    Create AULA_7.c
    constants 🚫

---
+++ AULA_7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    // Constant = valor fixo que não pode ser alterado pelo programa durante sua execução.
+
+    const float PI = 3.14159;
+
+    //PI = 420.69;  // Resultará em erro caso PI seja alterado.  
+    
+    printf("%f\n", PI);
+
+    return 0;
+}
