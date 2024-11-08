9eb78cd8acbd4d7cfcb921b1e3a43baf848579cb
João Victor <joaovictor.osantosdev@gmail.com>
2024-11-08 00:28:44 -0300

    Create AULA_01.cpp
    First C++ Program

---
+++ C++ Files/AULA_01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+
+    //  Isto é para comentar.
+    
+    /* 
+        E isto 
+        É para vários comentarios.
+    */
+
+    std::cout << "Olá, mundo!" << "\n";
+    std::cout << "Eu gosto de lasanha" << "\n";
+
+    return 0;
+}
