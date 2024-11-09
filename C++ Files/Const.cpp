79357b3d9d1aae0a98346aa03c5536e5a094983a
João Victor <joaovictor.osantosdev@gmail.com>
2024-11-09 17:23:41 -0300

    Add files via upload

---
+++ C++ Files/Const.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+    // A palavra-chave const especifica que o valor de uma variável é constante
+    // informa ao compilador para impedir qualquer modificação dela
+    // (somente leitura)
+
+int main(){
+
+    // Variaveis constates segundo a norma devem ser Maiúsculas.
+    
+    const double PI = 3.14159; 
+    const int VELOCIDADE_DA_LUZ = 299792458;
+    const int LARGURA = 1024;
+    const int ALTURA = 720;
+    
+    return 0;
+}
---
+++ C++ Files/VariablesAndDataTypes.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main(){
+
+    // Números Inteiros (int)
+    int age = 21;
+    int year = 2024;
+    int days = 9;
+
+    // Número com casas decimais (double)
+    double price = 30.99;
+    double gpa = 2.5;
+    double temperature = 31.1;
+
+    // Caracter único (char)
+    char grade = 'A';
+    char initial = 'J';
+    char currency = '$';
+
+    // Verdadeiro ou Falso (boolean)
+    bool student = true;
+    bool power = false;
+    bool forSale = false;
+
+    // Uma sequencia de caracteres, texto (string)
+    std::string name = "João Victor";
+    std::string day = "Fim de Semana";
+    std::string food = "Lasanha";
+    std::string address = "123 Far Far Way";
+    
+    std::cout << "Hello " << name << "\n";
+    std::cout << "Você tem " << age << " anos de idade" << "\n";
+
+    return 0;
+}
